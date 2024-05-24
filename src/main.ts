32535022ff995152bfc9e3b76bbd5e732384af01
oybek1024 <mukhiddinov.oybek1024@gmail.com>
2024-05-24 16:11:28 +0500

    cors disabled

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
